77611d31db1d1d88dff0c31152b8a9c9d45e5814
Salem Sama <samaexplosion@gmail.com>
2023-08-27 20:19:55 +0300

    27/8/2023

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Hello, World")
+
+
+def main():
+    name1 = input("Enter your name: ")
+    age1 = input("Enter your age: ")
+    phone1 = input("Enter your phone number: ")
+
+    call(name = name1, age = age1, phone = phone1)
+
+
+def call(**data):
+    print(data)
+    print(f"your name: {data['name']}\nyour age: {data['age']}\nyour Phone number: {data['phone']}")
+
+
+
+
+
+if __name__=="__main__":
+    main()
